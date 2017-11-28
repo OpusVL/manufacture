c2869118ecf2fc4adcc7a6fa5fb5bf6b2d58777e
Nick Booker <nick.booker@opusvl.com>
2017-11-28 11:42:12 +0000

    Fix post_init_hook to use new style (old style calling now broken)

--- quality_control_stock/__init__.py
+++ quality_control_stock/__init__.py
@@ -3,13 +3,13 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import models
-from openerp import SUPERUSER_ID
+
+from odoo import api, SUPERUSER_ID
 
 
 def post_init_hook(cr, registry):
     # Create QC triggers
-    picking_type_ids = registry['stock.picking.type'].search(
-        cr, SUPERUSER_ID, [])
-    for picking_type_id in picking_type_ids:
-        registry['stock.picking.type']._create_qc_trigger(
-            cr, SUPERUSER_ID, picking_type_id)
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    picking_types = env['stock.picking.type'].search([])
+    for picking_type in picking_types:
+        picking_type._create_qc_trigger()
